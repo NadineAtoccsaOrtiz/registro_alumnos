4bec150cd1989d2a83b398e0e358838749af4019
Nadine <nadineatoccsaortiz@gmail.com>
2023-12-05 12:22:40 -0500

    registrod e estudiantes

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import datetime
 #constantes para el tema oscuro
-COLOR_FONDO_PRIMARIO = '#004d4d'
-COLOR_FONDO_SECUNDARIRIO = '#b3dAff'
+COLOR_FONDO_PRIMARIO = '#5E0819'
+COLOR_FONDO_SECUNDARIRIO = '#8A349C'
 TITULO_APP = 'APP DE Nadine'
 date = datetime.datetime.now()
 HORA_ACTUAL = f"{date.day}-{date.month}-{date.year} Hora: {date.hour}:{date.minute}"
-COLOR_BOTON = '#43A6CD'
+COLOR_BOTON = '#EC8ECD'
 #FUNCION QUE NOS PERMITE CENTRAR NUESTRA PANTALLA
 def centrar_ventana(objeto, ancho_ventana, largo_ventana):
     pantalla_ancho = objeto.winfo_screenwidth()
